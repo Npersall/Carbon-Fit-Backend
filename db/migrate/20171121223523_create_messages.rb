241ab51dc59c2cd46e1d042e8d463ce788f1db73
Nathan <npersall@gmail.com>
2018-01-02 01:32:57 -0800

    deleted index.html

--- db/migrate/20171121223523_create_messages.rb
+++ db/migrate/20171121223523_create_messages.rb
@@ -1,7 +1,7 @@
 class CreateMessages < ActiveRecord::Migration[5.1]
   def change
     create_table :messages do |t|
-      t.string :Subject
+      t.string :subject
       t.string :body
 
       t.timestamps
